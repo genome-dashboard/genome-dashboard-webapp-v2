577c85814ecde42b0dfd2fdaf81f6cbbcc68dbdb
John Gentle <taoteg@gmail.com>
2019-07-22 13:45:09 -0500

    Completed refactoring into GenericViews.

--- polls/urls.py
+++ polls/urls.py
@@ -7,26 +7,26 @@ from . import views
 app_name = 'polls'
 
 # version 1.
-urlpatterns = [
-    ### ex: /polls/
-    path('', views.index, name='index'),
-
-    ### ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    ### Example were we added the word 'specifics'
-    ### path('specifics/<int:question_id>/', views.detail, name='detail'),
-
-    ### ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-
-    ### ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-]
-
-# version 2.
 # urlpatterns = [
-#     path('', views.IndexView.as_view(), name='index'),
-#     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-#     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+#     ### ex: /polls/
+#     path('', views.index, name='index'),
+#
+#     ### ex: /polls/5/
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     ### Example were we added the word 'specifics'
+#     ### path('specifics/<int:question_id>/', views.detail, name='detail'),
+#
+#     ### ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#
+#     ### ex: /polls/5/vote/
 #     path('<int:question_id>/vote/', views.vote, name='vote'),
 # ]
+
+# version 2.
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 # from django.http import Http404
 # from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 # from django.template import loader     # Used in version 3.
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
@@ -32,10 +33,19 @@ from .models import Choice, Question
 #     return HttpResponse(template.render(context, request))
 
 # version 4.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+# version 5: generic views.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 # DETAIL VIEW.
@@ -53,9 +63,32 @@ def index(request):
 #     return render(request, 'polls/detail.html', {'question': question})
 
 # version 3.
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# version 4: generic views.
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+# RESULTS VIEW.
+
+# version 1.
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+
+# version 2.
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+# version 3: generic views.
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 # VOTE VIEW.
@@ -82,16 +115,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-# RESULTS VIEW.
-
-# version 1.
-# def results(request, question_id):
-#     response = "You're looking at the results of question %s."
-#     return HttpResponse(response % question_id)
-
-# version 2.
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
