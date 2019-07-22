238d51ae0042c96805c95fbaf6f72c3eb3351a12
John Gentle <taoteg@gmail.com>
2019-07-22 14:23:01 -0500

    Changed default admin layout to tabular instead of stacked.

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
