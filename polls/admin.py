1e45b2e7ebfd43ffcbfb575c2d2dd869de7267d9
John Gentle <taoteg@gmail.com>
2019-07-22 14:22:05 -0500

    A beter way of adding choices to quesitons.

--- polls/admin.py
+++ polls/admin.py
@@ -2,15 +2,30 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-# Define model admin class.
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
+    # ver 1.
     # fields = ['pub_date', 'question_text']
+
+    # ver 2.
+    # fieldsets = [
+    #     (None,               {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date']}),
+    # ]
+
+    # ver 3.
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 # Register your models here.
-admin.site.register(Choice)
 admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
