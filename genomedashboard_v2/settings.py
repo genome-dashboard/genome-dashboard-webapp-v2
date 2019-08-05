f1c7cdd56ea83c65fda9d7892395e36c01332036
John Gentle <taoteg@gmail.com>
2019-08-05 15:47:36 -0500

    Tweaking templating system to pickup static assets properly.

--- genomedashboard_v2/settings.py
+++ genomedashboard_v2/settings.py
@@ -140,10 +140,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+# STATIC_ROOT is the location you wasnt to serve your files from AFTER you run python manage.py collectstatic.
+
+# Development paths.
+# STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Production paths.
+# STATIC_ROOT = "/var/www/example.com/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, 'static'),
+    # Add specific app static resources here.
+    # Remember to run: python manage.py collectstatic.
+    os.path.join(BASE_DIR, 'genomedashboard_v2/static'),
+    os.path.join(BASE_DIR, 'polls/static'),
 ]
 
 STATICFILES_FINDERS = [
--- genomedashboard_v2/urls.py
+++ genomedashboard_v2/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+# Only used during dev for serving static assets.
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -23,4 +27,4 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
