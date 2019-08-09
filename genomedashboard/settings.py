4760c8020d62414779ec45763a39be31946f3ec2
John Gentle <taoteg@gmail.com>
2019-08-09 14:54:44 -0500

    Refactored into a semi-working state but cannto load any satic resources correctly. Hmmm.

--- genomedashboard/settings.py
+++ genomedashboard/settings.py
@@ -1,3 +1,5 @@
+# genomedashboard/settings.py
+
 """
 Django settings for genomedashboard project.
 
@@ -10,18 +12,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-# Imports and module dependencies.
 import os
 import logging
-
-
-# Secrets file.
 from genomedashboard import secrets
 
+def gettext(s): return s
 
-# Helper methods.
-gettext = lambda s: s
-
+# Build paths relative to the project root like this: os.path.join(DATA_DIR, ...)
+DATA_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,71 +28,137 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bv6*hn@0g$qg-2=vjc@rfu$g7vnwz#+o&fpjevnh_ky3@bs=of'
+SECRET_KEY = secrets._SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = secrets._DEBUG
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = secrets._ALLOWED_HOSTS
 
 # Application definition
+ROOT_URLCONF = 'genomedashboard.urls'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'genomedashboard', 'static'),
+    os.path.join(BASE_DIR, 'polls', 'static'),
+    os.path.join(BASE_DIR, 'dashboard', 'static'),
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'genomedashboard', 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                # 'django.template.context_processors.i18n',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.media',
+                'django.template.context_processors.csrf',
+                'django.template.context_processors.tz',
+                # 'sekizai.context_processors.sekizai',
+                'django.template.context_processors.static',
+                # 'cms.context_processors.cms_settings'
+            ],
+            # WARNING: You cannot use 'loaders' and 'APP_DIRS' at the same time!!!
+            # 'loaders': [
+            #     'django.template.loaders.filesystem.Loader',
+            #     'django.template.loaders.app_directories.Loader',
+            #     'django.template.loaders.eggs.Loader'
+            # ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    # 'cms.middleware.utils.ApphookReloadMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'cms.middleware.user.CurrentUserMiddleware',
+    # 'cms.middleware.page.CurrentPageMiddleware',
+    # 'cms.middleware.toolbar.ToolbarMiddleware',
+    # 'cms.middleware.language.LanguageCookieMiddleware'
+]
 
 INSTALLED_APPS = [
     # Django.
-    'django.contrib.admin',
+    # 'djangocms_admin_style',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
+    'django.contrib.messages',
+
+    # CMS (optional).
+    # 'cms',
+    # 'menus',
+    # 'sekizai',
+    # 'treebeard',  # Replaces mptt.
+    # 'djangocms_text_ckeditor',
+    # 'filer',
+    # 'easy_thumbnails',
+    # 'djangocms_audio',   # Doesn't work right.
+    # 'djangocms_column',
+    # 'djangocms_file',
+    # 'djangocms_link',
+    # 'djangocms_picture',
+    # 'djangocms_style',
+    # 'djangocms_snippet',
+    # 'djangocms_googlemap',
+    # 'djangocms_video',
+    # 'djangocms_icon',
+    # 'djangocms_bootstrap4',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_alerts',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_badge',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_card',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_carousel',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_collapse',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_content',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_grid',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_jumbotron',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_link',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_listgroup',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_media',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_picture',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_tabs',
+    # 'djangocms_bootstrap4.contrib.bootstrap4_utilities',
+
     # Custom apps.
     'genomedashboard.apps.GenomedashboardConfig',
     'polls.apps.PollsConfig',
     'dashboard.apps.DashboardConfig'
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-ROOT_URLCONF = 'genomedashboard.urls'
-WSGI_APPLICATION = 'genomedashboard.wsgi.application'
-GATEWAY_NAMESPACE = secrets._GATEWAY_NAMESPACE
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        # sqlite3
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-        # Postgres
         'ENGINE': secrets._DJANGO_DB_ENGINE,
         'NAME': secrets._DJANGO_DB_NAME,
         'USER': secrets._DJANGO_DB_USER,
@@ -104,6 +168,32 @@ DATABASES = {
     }
 }
 
+MIGRATION_MODULES = {
+
+}
+
+# SSL Setup.
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
+
+LANGUAGE_CODE = 'en'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+LANGUAGES = (
+    # Customize this if needed.
+    ('en', gettext('en')),
+)
+
+WSGI_APPLICATION = 'genomedashboard.wsgi.application'
+GATEWAY_NAMESPACE = secrets._GATEWAY_NAMESPACE
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -123,77 +213,62 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
-# STATIC_ROOT is the location you wasnt to serve your files from AFTER you run python manage.py collectstatic.
-
-# Development paths.
-# STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_srv')
-
-# Production paths.
-# STATIC_ROOT = "/var/www/example.com/static/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    # Add specific app static resources here.
-    # Remember to run: python manage.py collectstatic.
-    os.path.join(BASE_DIR, 'genomedashboard/static'),
-    os.path.join(BASE_DIR, 'polls/static'),
-]
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
-
-
-# Media files.
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
-
-
-# Internationalization.
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-LANGUAGE_CODE = 'en'   # 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
-
-LANGUAGES = (
-    ('en', gettext('en')),
-)
-
-
 # Logging./
 logger = logging.getLogger(__file__)
 
+# DjangoCMS configuration.
+# Commented out because CMS is not installed.
+"""
+SITE_ID = secrets._SITE_ID
+
+CMS_LANGUAGES = {
+    # Customize this
+    1: [
+        {
+            'code': 'en',
+            'name': gettext('en'),
+            'redirect_on_fallback': True,
+            'public': True,
+            'hide_untranslated': False,
+        },
+    ],
+    'default': {
+        'redirect_on_fallback': True,
+        'public': True,
+        'hide_untranslated': False,
+    },
+}
+
+CMS_TEMPLATES = secrets._CMS_TEMPLATES
+CMS_PERMISSION = True
+CMS_PLACEHOLDER_CONF = {}
+
+THUMBNAIL_HIGH_RESOLUTION = True
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters'
+)
+"""
+
+DJANGOCMS_PICTURE_NESTING = True
+DJANGOCMS_PICTURE_RESPONSIVE_IMAGES = True
+DJANGOCMS_PICTURE_RATIO = 1.618
+# Custom picture templates - if required.
+# DJANGOCMS_PICTURE_TEMPLATES = [
+#     ('background', _('Background image')), # Need to design these first!
+# ]
+
+DJANGOCMS_AUDIO_ALLOWED_EXTENSIONS = ['mp3', 'ogg', 'wav']
+# Custom audio templates - if required.
+# DJANGOCMS_AUDIO_TEMPLATES = [
+#     ('feature', _('Featured Version')),
+# ]
 
 # Google Analytics.
 GOOGLE_ANALYTICS_PROPERTY_ID = secrets._GOOGLE_ANALYTICS_PROPERTY_ID
 GOOGLE_ANALYTICS_PRELOAD = secrets._GOOGLE_ANALYTICS_PRELOAD
-
 
 # Exported settings.
 SETTINGS_EXPORT = [
--- genomedashboard/urls.py
+++ genomedashboard/urls.py
@@ -1,32 +1,44 @@
-"""genomedashboard URL Configuration
+# -*- coding: utf-8 -*-
+"""genomedashboard URL Configuration"""
+from __future__ import absolute_import, print_function, unicode_literals
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
-
-# Only used during dev for serving static assets.
+# from cms.sitemaps import CMSSitemap
+# from django.conf.urls import include, url
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from . import views
 
+admin.autodiscover()
+
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('', views.IndexView.as_view(), name='index'),
-    # path('polls/', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # <-- ONLY USE THIS IN DEVELOPMENT.
-# ]         # <-- USE THIS IN PRODUCTION, STATIC ASSETS WILL BE SERVED FROM THE STATIC_ROOT.
+# ]
+# This is only needed when using runserver. Do not use this in production.
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns += [
+#     url(r'^sitemap\.xml$', sitemap,
+#         {'sitemaps': {'cmspages': CMSSitemap}}),
+# ]
+
+# urlpatterns += i18n_patterns(
+#     re_path(r'^admin/', admin.site.urls),  # NOQA
+#     re_path(r'^', include('cms.urls')),
+# )
+
+# This is only needed when using runserver.
+# if settings.DEBUG:
+#     urlpatterns = [
+#         re_path(r'^media/(?P<path>.*)$', serve,
+#             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+#         ] + staticfiles_urlpatterns() + urlpatterns
